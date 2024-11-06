b94620643493a63f35f89de976792d18047bf4cc
unknown <vselabs@ENGR1506-07.mesa.gmu.edu>
2024-11-06 18:07:31 -0500

    added more stuff

--- scriptFiles/script.js
+++ scriptFiles/script.js
@@ -1,45 +1,62 @@
 let reportCounter = 0;
-
+let assignDJCounter = 0;
 /**
  * method to display the report whenever it gets one.
  * @param {to keep track of the current report, will be changed to stored in data base later} reportCounter
  */
 function generateReportTable(reportCounter) {
-  if (reportCounter == 6) return;
-  setTimeout(() => {
-    reportCounter++;
+    if (reportCounter == 6) return;
+    setTimeout(() => {
+        reportCounter++;
 
-    let tr = document.createElement("tr");
+        let tr = document.createElement("tr");
 
-    //first part of the row.
-    let td1 = document.createElement("td");
-    let span1 = document.createElement("span");
-    let p1 = document.createElement("p");
-    p1.className = "dateOfAssign";
-    p1.textContent = "mm/dd/yyyy";
-    span1.appendChild(p1);
-    td1.textContent = "song was Assigned by Producer ";
-    td1.appendChild(span1);
-    tr.appendChild(td1);
+        //first part of the row.
+        let td1 = document.createElement("td");
+        let span1 = document.createElement("span");
+        let p1 = document.createElement("p");
+        p1.className = "dateOfAssign";
+        p1.textContent = "mm/dd/yyyy";
+        span1.appendChild(p1);
+        td1.textContent = "song was Assigned by Producer ";
+        td1.appendChild(span1);
+        tr.appendChild(td1);
 
-    //second part of the row
+        //second part of the row
 
-    let td2 = document.createElement("td");
-    td2.textContent = "DJ did not played the song ";
-    let button = document.createElement("button");
-    let span2 = document.createElement("span");
+        let td2 = document.createElement("td");
+        td2.textContent = "DJ did not played the song ";
+        let button = document.createElement("button");
+        let span2 = document.createElement("span");
 
-    button.id = "reportViewDetialsButton";
-    button.textContent = "view details";
+        button.id = "reportView";
+        button.textContent = "view details";
 
-    span2.appendChild(button);
-    td2.appendChild(span2);
-    tr.appendChild(td2);
-    const viewAll = document.getElementById("viewAll");
-    document.getElementById("reportTable").insertBefore(tr, viewAll);
+        span2.appendChild(button);
+        td2.appendChild(span2);
+        tr.appendChild(td2);
+        const viewAll = document.getElementById("viewAll");
+        document.getElementById("reportTable").insertBefore(tr, viewAll);
+        //This is an example of Listener Approach.
+        button.addEventListener("click", () => {
+            document.getElementById("details-box").style.display = "block";
+        });
 
-    generateReportTable(reportCounter);
-  }, 500);
+
+
+        generateReportTable(reportCounter);
+    }, 500);
 }
 
+document.getElementById("details-button").addEventListener("click", () => {
+    document.getElementById("details-box").style.display = "none";
+})
+
+
+function generateAssignDJ() {
+
+}
+
+
 generateReportTable(reportCounter);
+displayDetails();
