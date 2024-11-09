fa3f1f926879127356de744d683e275d38a869a9
Hayder Atiyah <eng.hayderAtiyah@gmail.com>
2024-11-08 23:47:47 -0500

    page is ready for project 3

--- scriptFiles/questions.js
+++ scriptFiles/questions.js
@@ -1,11 +1,10 @@
-// questions.js
 export default class Questions {
     constructor() {
         this.qaList = [
             { question: "Assign new Dj? ", answer: "simply click on edit key and you will be able to assign new Dj" },
             { question: "save my changes?", answer: "click on the Apply Button" },
             { question: "shortcut to apply changes? ", answer: "Yes, Click left shift +  enter." },
-            { question: "shortcut to search? ", answer: "Yes, Click left ctrl + F" },
+            { question: "shortcut to search? ", answer: "Yes, Click left shift + F" },
             { question: "delete a assigned DJ? ", answer: "Yes, Click on the delete button." },
         ];
     }
