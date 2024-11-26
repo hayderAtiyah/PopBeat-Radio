20085143d8ebf7b57fd971140b67c80a4f7b8b30
unknown <vselabs@ENGR1506-07.mesa.gmu.edu>
2024-11-25 19:54:55 -0500

    passing data to ejs

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
-
-
 app.use(express.static('public'));
 
-// app.get('', (request, response) => {
-//     response.send('hello')
-// })
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', './views/pages');
+
+app.locals.managerTitleName = "mana"
+
+app.get("/manager", (req, res) => {
+    res.render('manager', { managerTitleName: "hss", taylorPicSrc: "/photos/taylorWallpaper.jpg", mangamentTitle: "PopBeat Radio Mangament" })
+});
+
+
 
 app.listen(3500, () => {
     console.log("running")
